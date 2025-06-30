6c46d85572c35d2980788bd55f290939368c46a0
edgamer63 <edua.pastene@duocuc.cl>
2025-06-29 22:34:20 -0400

    Corrigiendo integracion controller test

--- administradorSistema/src/test/java/Microservicio/de/Administracion/del/Sistema/controller/IntegracionControllerTest.java
+++ administradorSistema/src/test/java/Microservicio/de/Administracion/del/Sistema/controller/IntegracionControllerTest.java
@@ -1,28 +1,22 @@
 package Microservicio.de.Administracion.del.Sistema.controller;
 
-import Microservicio.de.Administracion.del.Sistema.model.Usuario;
-import Microservicio.de.Administracion.del.Sistema.service.AdminService;
+import Microservicio.de.Administracion.del.Sistema.Integracion.IntegracionController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AdminController.class)
+@WebMvcTest(IntegracionController.class)
 public class IntegracionControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private AdminService adminService;
 
     @Autowired
     private ObjectMapper objectMapper;
