866389570949882ad684abb1311892aaaea4ead7
edgamer63 <edua.pastene@duocuc.cl>
2025-06-29 22:47:20 -0400

    Solución Integracion controller test

--- administradorSistema/src/test/java/Microservicio/de/Administracion/del/Sistema/controller/IntegracionControllerTest.java
+++ administradorSistema/src/test/java/Microservicio/de/Administracion/del/Sistema/controller/IntegracionControllerTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(IntegracionController.class)
 public class IntegracionControllerTest {
@@ -21,16 +24,23 @@ public class IntegracionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private RestTemplate restTemplate;  // <<--- ESTO ES LO IMPORTANTE
+
     @BeforeEach
     void setUp() {
     }
 
     @Test
     public void testIntegracion() throws Exception {
-        // Ejecutar petición
+        // Simular la respuesta del RestTemplate
+        when(restTemplate.getForObject("http://127.0.0.1:8081/facturas", String.class))
+                .thenReturn("Factura simulada");
+
         mockMvc.perform(get("/listarFactura")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("Factura simulada"));
     }
 }
